2fd214a14c56c4e9139a2a374591953f2cce96b8
hedwig100 <pentagon7951@outlook.jp>
2021-04-02 15:42:11 +0900

    bugfix

--- colab.js
+++ colab.js
@@ -1,5 +1,5 @@
 function ClickConnect(){
-    document.querySelector("colab-connect-button").click(); 
-    console.log("click the button!")
-  }
-  setInterval(ClickConnect,1000*60); // "1分ごとにクリック"
+  document.querySelector("colab-connect-button").click(); 
+  console.log("click the button!")
+}
+setInterval(ClickConnect,1000*60); // "1分ごとにクリック"
